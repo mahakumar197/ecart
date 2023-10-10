dd37bc27e09f058d8586ba5bd1c4cc1e52686433
maha <mahakumar197@gmail.com>
2023-10-10 12:42:34 +0530

    test

---
+++ App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/navBlock/Nav";
+// import Nav from "./components/navBlock/nav";
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/navBlock/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="App">
+      <div cla>nav</div>
+    </div>
+  );
+}
+
+export default Nav;
